1c2b9aa3a2bc93a80ec1aed70f80b346c07633f9
joseeetm <josillo2002@gmail.com>
2023-07-30 20:50:19 +0200

    Add lists-2d
    Simple list 2d and printing 2 indexes

---
+++ Learning Python 3- codecademy/Control flow/lists-2d.py
@@ -0,0 +1,9 @@
+# 2d list with test scores from a classroom
+class_name_test = [["Jenny", 90], ["Alexus", 85.5], ["Sam", 83], ["Ellie", 101.5]]
+print(class_name_test)
+# Variable with value of Sam score (selecting the third list and the second index from that one)
+sams_score = class_name_test[2][1]
+print(sams_score)
+# Variable with value of ellie score using negative values
+ellies_score = class_name_test[-1][-1]
+print(ellies_score)
