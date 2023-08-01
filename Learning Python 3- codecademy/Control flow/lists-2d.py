4b649efcd967fc0e7d5e4d5704ffbbb92106fc46
joseeetm <josillo2002@gmail.com>
2023-08-01 22:55:08 +0200

    Update lists-2d.py
    Add new list2d and updating it

--- Learning Python 3- codecademy/Control flow/lists-2d.py
+++ Learning Python 3- codecademy/Control flow/lists-2d.py
@@ -7,3 +7,11 @@ print(sams_score)
 # Variable with value of ellie score using negative values
 ellies_score = class_name_test[-1][-1]
 print(ellies_score)
+
+# 2d list with name, nationality and grade level of students
+incoming_class = [["Kenny","American",9], ["Tanya","Ukrainian",9],["Madison","Indian",7]]
+# Update Madison grade to 8 using positive indexes
+incoming_class[2][2] = 8
+# Update Kenny name using negative indexes
+incoming_class[-3][-3] = "Ken"
+print(incoming_class)
