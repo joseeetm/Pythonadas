2652d43251b535e6fc78cbbdfdd76d7bfff219ff
joseeetm <josillo2002@gmail.com>
2024-06-11 21:26:46 +0200

    Add builtin functions

---
+++ Learning Python 3- codecademy/Functions/built-in_function.py
@@ -0,0 +1,14 @@
+
+tshirt_price = 9.75
+shorts_price = 15.50
+mug_price = 5.99
+poster_price = 2.00
+
+max_price = max(tshirt_price, shorts_price, mug_price , poster_price)
+print(max_price)
+
+min_price = min(tshirt_price, shorts_price, mug_price , poster_price)
+print(min_price)
+
+rounded_price = round(tshirt_price,1)
+print(rounded_price)
