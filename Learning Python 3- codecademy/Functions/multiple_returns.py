681338f97ab5b230d9f6595e48c1e178e8d3d8a9
joseeetm <josillo2002@gmail.com>
2024-08-17 21:23:48 +0200

    Add multiple_returns.py

---
+++ Learning Python 3- codecademy/Functions/multiple_returns.py
@@ -0,0 +1,23 @@
+# Define la función top_tourist_locations_italy
+def top_tourist_locations_italy():
+  # Asigna "Rome" a la variable first
+  first = "Rome"
+  # Asigna "Venice" a la variable second
+  second = "Venice"
+  # Asigna "Florence" a la variable third
+  third = "Florence"
+  # Retorna las tres variables
+  return first, second, third
+
+# Llama a la función y asigna los valores retornados a las variables most_popular1, most_popular2, y most_popular3
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+
+# Imprime la tupla retornada por la función
+print(top_tourist_locations_italy())
+
+# Imprime el valor de most_popular1
+print(most_popular1)
+# Imprime el valor de most_popular2
+print(most_popular2)
+# Imprime el valor de most_popular3
+print(most_popular3)
